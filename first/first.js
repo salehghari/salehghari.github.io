843c471204a9801018409c5acebed1ccd74a1f39
Saleh <salehghari8470@gmail.com>
2022-01-08 18:23:19 +0330

    Added 'Home' and 'About' pages, fixed bug.

--- first/first.js
+++ first/first.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector("#check");
 const sideMenu = document.querySelector(".sideMenu");
+const label = document.querySelector("#label");
 
 menuIcon.addEventListener("click", e => {
     if (menuIcon.checked == true){
@@ -8,3 +9,10 @@ menuIcon.addEventListener("click", e => {
         sideMenu.style.left = "-100%";
     }
 });
+var width;
+window.onresize = window.onload = function() {
+    width = this.innerWidth;
+    if (width >= 901) {
+        sideMenu.style.left = "-100%";
+    }
+}
