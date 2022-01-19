004f87ca651f086870b81a306229a55fb39a2695
Saleh <salehghari8470@gmail.com>
2022-01-19 03:53:25 +0330

    Added 'Projects' page, created a slideshow for my projects
    I have changed the background of 'Skills' and 'Projects' page to a awesome gradient!
    The 'Projects' page is not responsive yet.

--- first/first.js
+++ first/first.js
@@ -16,3 +16,53 @@ window.onresize = window.onload = function() {
         sideMenu.style.left = "-100%";
     }
 }
+// functions
+function classSwitcher() {
+    // slides
+    slides.forEach(slide => slide.classList.remove("active"))
+    slides[active].classList.add("active")
+    // points
+    points.forEach(point => point.classList.remove("active"))
+    points[active].classList.add("active")
+}
+
+let goNext = () => {
+    active = (active == slides.length - 1) ? 0 : active + 1
+    classSwitcher()
+}
+
+let goPrev = () => {
+    active = (active == 0) ? slides.length - 1 : active - 1
+    classSwitcher()
+}
+
+// variables
+let active = 0
+let timer = 3000
+let slideshow = document.querySelector(".slideShow")
+let slides = document.querySelectorAll(".slide")
+let points = document.querySelectorAll(".points > span")
+let prev = document.querySelector(".prev")
+let next = document.querySelector(".next")
+
+// setInterval
+let runSlideshow = setInterval(goNext, timer)
+
+// point event
+points.forEach((point, index) => {
+    point.addEventListener("click", e => {
+        active = index
+        classSwitcher()
+    })
+})
+
+// next event
+next.addEventListener("click", e => goNext())
+// previous event
+prev.addEventListener("click", e => goPrev())
+
+// mouseover
+slideshow.addEventListener("mouseover", e => clearInterval(runSlideshow))
+// mouseleave
+slideshow.addEventListener("mouseleave", e => runSlideshow = setInterval(goNext, timer))
+classSwitcher()
